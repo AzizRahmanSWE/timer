cc622a64b3a29ed94a186292b0ffb8d6f4bce6ee
Friies <bahram.parsa2019@gmail.com>
2023-03-19 22:06:52 -0400

    added the timer1.js which when given an array of numbers, a dot(beep) will be seen(heard) in the terminal

---
+++ timer1.js
@@ -0,0 +1,11 @@
+const timer = process.argv.slice(2); //get rid of the two initial process.argv lines.
+// process.stdout.write('\x07');
+// args is used to refer to an array of command line arguments
+
+for (let i = 0; i < timer.length; i++) {
+  if (timer[i] > 0 && !isNaN(timer[i])) { //checks if the argument is a number greater than 0 and is not not a number.
+    setTimeout(() => {
+      process.stdout.write('.'); //the sound was unconsistent on my machine so I used a dot instead.
+    }, timer[i] * 1000);
+  }
+}
