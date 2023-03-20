f744ed6533c530e5cf8c0c926208f58fb9fe7efe
Friies <bahram.parsa2019@gmail.com>
2023-03-19 22:07:46 -0400

    fixed a couple spelling errors.

--- timer1.js
+++ timer1.js
@@ -1,6 +1,6 @@
 const timer = process.argv.slice(2); //get rid of the two initial process.argv lines.
 // process.stdout.write('\x07');
-// args is used to refer to an array of command line arguments
+// timer is used to refer to an array of command line arguments
 
 for (let i = 0; i < timer.length; i++) {
   if (timer[i] > 0 && !isNaN(timer[i])) { //checks if the argument is a number greater than 0 and is not not a number.
